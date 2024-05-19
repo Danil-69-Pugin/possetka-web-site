42b490828a2f985a7c328e48751bfebdbb137940
Danil-69-Pugin <danil2017pugin@gmail.com>
2024-05-19 14:27:33 +0300

    v 2.1

--- src/Components/optoviki.js
+++ src/Components/optoviki.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import "../Styles/optoviki.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCommentDots} from "@fortawesome/free-solid-svg-icons";
 
 function Optoviki() {
+    const [isButtonDisabled] = useState(false);
     return (
         <div className="optoviki-section" id="optoviki">
                 <div>
@@ -9,9 +12,21 @@ function Optoviki() {
                     <h3 className="Optoviki-title">
                         <span>ВЫ ОПТОВЫЙ ПОКУПАТЕЛЬ ИЛИ МОНТАЖНАЯ БРИГАДА?</span>
                     </h3>
-                        <div>
-                            <p>Получите специальные условия на всю нашу продукцию!</p>Получить предложение!
+                    <br/>
+                    <br/><br/><br/>
+                        <div className="Optoviki-text">
+                            <p>Получите специальные условия на всю нашу продукцию!</p><br/><br/><br/><br/>
                         </div>
+                    <div className="optoviki-btn">
+                        <a href="tel:+73422502202">      <button
+                            className="navbar-btn"
+                            type="button"
+                            disabled={isButtonDisabled}
+
+                        >
+                            <FontAwesomeIcon icon={faCommentDots} /> Получить предложение
+                        </button></a></div>
+                    <br/><br/><br/><br/><br/><br/>
                     </div>
         </div>
     )
