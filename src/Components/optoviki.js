e33a96eb8886a0d4f69c88935762d2659c2f36b8
Danil-69-Pugin <danil2017pugin@gmail.com>
2024-05-19 13:38:35 +0300

    v 2.0

---
+++ src/Components/optoviki.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../Styles/optoviki.css";
+
+function optoviki() {
+    return (
+        <div className="optoviki-section" id="optoviki">
+        </div>
+    )
+}
